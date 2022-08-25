97df3aa2006322eee6e89992952cc362d3bdaa8d
Arielly <arielly.oliveira@padolabs.com.br>
2022-08-25 12:56:54 -0300

    imc ok

--- aula-1/calculaIMC/app/src/main/java/com/example/calculaimc/MainActivity.kt
+++ aula-1/calculaIMC/app/src/main/java/com/example/calculaimc/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculaimc
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,6 +8,7 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,15 +20,18 @@ class MainActivity : AppCompatActivity() {
         val btnMulher = findViewById<Button>(R.id.btnMulher)
         val btnHomem = findViewById<Button>(R.id.btnHomem)
         val btnOk = findViewById<Button>(R.id.btnOk)
+        val textSexo = findViewById<TextView>(R.id.textSexo)
 
         var click = 0;
 
         btnMulher.setOnClickListener {
             click = 1;
+            textSexo.setText("Sou uma mulher");
         }
 
         btnHomem.setOnClickListener {
             click = 2;
+            textSexo.setText("Sou um homem");
         }
 
         btnOk.setOnClickListener {
@@ -37,157 +42,155 @@ class MainActivity : AppCompatActivity() {
                 val calculaIMC = quilos / (altura * altura);
 
                 if(click == 1) {
-                    click = 0;
                     if(idade < 18) {
                         textIMC.setText("Calculamos somente IMC de adultos!");
                     } else if(idade >= 18 && idade <= 24) {
                         if(calculaIMC < 19) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso baixo)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso baixo)");
                         } else if(calculaIMC >= 19 && calculaIMC <= 24.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso normal)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso normal)");
                         } else if(calculaIMC >= 25 && calculaIMC <= 29.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Pré obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Pré obesidade)");
                         } else if(calculaIMC >= 30 && calculaIMC <= 39.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade)");
                         } else if(calculaIMC > 39.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade grave)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade grave)");
                         }
                     } else if(idade >= 25 && idade <= 34) {
                         if(calculaIMC < 20) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso baixo)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso baixo)");
                         } else if(calculaIMC >= 20 && calculaIMC <= 25.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso normal)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso normal)");
                         } else if(calculaIMC >= 26 && calculaIMC <= 30.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Pré obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Pré obesidade)");
                         } else if(calculaIMC >= 31 && calculaIMC <= 40.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade)");
                         } else if(calculaIMC > 40.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade grave)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade grave)");
                         }
                     } else if(idade >= 35 && idade <= 44) {
                         if(calculaIMC < 21) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso baixo)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso baixo)");
                         } else if(calculaIMC >= 21 && calculaIMC <= 26.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso normal)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso normal)");
                         } else if(calculaIMC >= 27 && calculaIMC <= 31.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Pré obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Pré obesidade)");
                         } else if(calculaIMC >= 32 && calculaIMC <= 41.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade)");
                         } else if(calculaIMC > 41.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade grave)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade grave)");
                         }
                     } else if(idade >= 45 && idade <= 54) {
                         if(calculaIMC < 22) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso baixo)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso baixo)");
                         } else if(calculaIMC >= 22 && calculaIMC <= 27.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso normal)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso normal)");
                         } else if(calculaIMC >= 28 && calculaIMC <= 32.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Pré obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Pré obesidade)");
                         } else if(calculaIMC >= 33 && calculaIMC <= 42.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade)");
                         } else if(calculaIMC > 42.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade grave)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade grave)");
                         }
                     } else if(idade >= 55 && idade <= 64) {
                         if(calculaIMC < 23) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso baixo)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso baixo)");
                         } else if(calculaIMC >= 23 && calculaIMC <= 28.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso normal)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso normal)");
                         } else if(calculaIMC >= 29 && calculaIMC <= 33.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Pré obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Pré obesidade)");
                         } else if(calculaIMC >= 34 && calculaIMC <= 43.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade)");
                         } else if(calculaIMC > 43.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade grave)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade grave)");
                         }
                     } else {
                         if(calculaIMC < 24) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso baixo)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso baixo)");
                         } else if(calculaIMC >= 24 && calculaIMC <= 29.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso normal)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso normal)");
                         } else if(calculaIMC >= 30 && calculaIMC <= 34.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Pré obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Pré obesidade)");
                         } else if(calculaIMC >= 35 && calculaIMC <= 44.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade)");
                         } else if(calculaIMC > 44.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade grave)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade grave)");
                         }
                     }
                 } else if(click == 2) {
-                    click = 0;
                     if(idade < 18) {
                         textIMC.setText("Calculamos somente IMC de adultos!");
                     } else if(idade >= 18 && idade <= 24) {
                         if(calculaIMC < 20) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso baixo)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso baixo)");
                         } else if(calculaIMC >= 20 && calculaIMC <= 25.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso normal)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso normal)");
                         } else if(calculaIMC >= 26 && calculaIMC <= 30.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Pré obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Pré obesidade)");
                         } else if(calculaIMC >= 31 && calculaIMC <= 40.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade)");
                         } else if(calculaIMC > 40.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade grave)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade grave)");
                         }
                     } else if(idade >= 25 && idade <= 34) {
                         if(calculaIMC < 21) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso baixo)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso baixo)");
                         } else if(calculaIMC >= 21 && calculaIMC <= 26.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso normal)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso normal)");
                         } else if(calculaIMC >= 27 && calculaIMC <= 31.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Pré obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Pré obesidade)");
                         } else if(calculaIMC >= 32 && calculaIMC <= 41.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade)");
                         } else if(calculaIMC > 41.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade grave)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade grave)");
                         }
                     } else if(idade >= 35 && idade <= 44) {
                         if(calculaIMC < 22) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso baixo)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso baixo)");
                         } else if(calculaIMC >= 22 && calculaIMC <= 27.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso normal)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso normal)");
                         } else if(calculaIMC >= 28 && calculaIMC <= 32.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Pré obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Pré obesidade)");
                         } else if(calculaIMC >= 33 && calculaIMC <= 42.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade)");
                         } else if(calculaIMC > 42.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade grave)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade grave)");
                         }
                     } else if(idade >= 45 && idade <= 54) {
                         if(calculaIMC < 23) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso baixo)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso baixo)");
                         } else if(calculaIMC >= 23 && calculaIMC <= 28.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso normal)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso normal)");
                         } else if(calculaIMC >= 29 && calculaIMC <= 33.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Pré obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Pré obesidade)");
                         } else if(calculaIMC >= 34 && calculaIMC <= 43.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade)");
                         } else if(calculaIMC > 43.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade grave)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade grave)");
                         }
                     } else if(idade >= 55 && idade <= 64) {
                         if(calculaIMC < 24) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso baixo)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso baixo)");
                         } else if(calculaIMC >= 24 && calculaIMC <= 29.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso normal)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso normal)");
                         } else if(calculaIMC >= 30 && calculaIMC <= 34.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Pré obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Pré obesidade)");
                         } else if(calculaIMC >= 35 && calculaIMC <= 44.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade)");
                         } else if(calculaIMC > 44.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade grave)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade grave)");
                         }
                     } else {
                         if(calculaIMC < 25) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso baixo)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso baixo)");
                         } else if(calculaIMC >= 25 && calculaIMC <= 30.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Peso normal)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Peso normal)");
                         } else if(calculaIMC >= 31 && calculaIMC <= 35.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Pré obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Pré obesidade)");
                         } else if(calculaIMC >= 36 && calculaIMC <= 45.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade)");
                         } else if(calculaIMC > 45.9) {
-                            textIMC.setText("Seu IMC é " + calculaIMC + " (Obesidade grave)");
+                            textIMC.setText("Seu IMC é ${String.format("%.2f", calculaIMC)} (Obesidade grave)");
                         }
                     }
                 } else {
