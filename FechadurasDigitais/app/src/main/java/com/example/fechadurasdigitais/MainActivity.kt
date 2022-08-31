a6d6e23369ee1ffe6ea82097a79b4ffb43c16f44
Arielly <arielly.oliveira@padolabs.com.br>
2022-08-31 17:43:42 -0300

    aula 3

--- FechadurasDigitais/app/src/main/java/com/example/fechadurasdigitais/MainActivity.kt
+++ FechadurasDigitais/app/src/main/java/com/example/fechadurasdigitais/MainActivity.kt
@@ -1,20 +1,15 @@
-package com.example.digitallockingpado
+package com.example.fechadurasdigitais
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
 import android.widget.Toast
 import com.google.android.material.appbar.MaterialToolbar
-import com.example.digitallockingpado.Fde600wPage as Fde600wPage1
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val btn = findViewById<Button>(R.id.btn)
         val mainToolBar = findViewById<MaterialToolbar>(R.id.mainToolBar)
 
         mainToolBar.setNavigationOnClickListener {
@@ -23,7 +18,6 @@ class MainActivity : AppCompatActivity() {
                 "Hamburger pressionado!!!",
                 Toast.LENGTH_SHORT
             ).show()
-            Log.e("app", "menu click")
         }
 
         mainToolBar.setOnMenuItemClickListener { menuItem ->
@@ -49,9 +43,5 @@ class MainActivity : AppCompatActivity() {
                 else -> false
             }
         }
-
-//        btn.setOnClickListener {
-//            startActivity(Intent(this, Fde600wPage1::class.java))
-//        }
     }
 }
--- aula-2/fechadurasDigitais/app/src/main/java/com/example/digitallockingpado/Fde600wPage.kt
+++ aula-2/fechadurasDigitais/app/src/main/java/com/example/digitallockingpado/Fde600wPage.kt
@@ -1,7 +1,5 @@
 package com.example.digitallockingpado
 
-import android.app.ProgressDialog.show
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
