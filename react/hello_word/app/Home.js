8befec2fb886fbfa3504c70c3624f9be781f7306
Arielly <arielly.oliveira@padolabs.com.br>
2022-11-30 17:41:16 -0300

    tela de clima

--- react/hello_word/app/Home.js
+++ react/hello_word/app/Home.js
@@ -1,66 +1,76 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { styles } from './style';
 
 function Weather() {
-  return (
-    <View style={styles.cabecalho}>
-        <View style={styles.cabecalho}>
-            <Text>Accra, Ghana</Text>
-            <Text>20-Sep-22</Text>
-        </View>
-        <View style={styles.cabecalho}>
-            <Text>20°</Text>
-            <Text>Real feel: 20</Text>
-            <Text>Cloudy</Text>
-        </View>
-        <View style={styles.cabecalho}>
-            <Text>UV Index</Text>
-            <View style={styles.cabecalho}>
-                <Text>Humidity</Text>
-                <Text>Wind Speed</Text>
-                <Text>Rain Probability</Text>
-                <Text>Pressure</Text>
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.location}>
+                    <Image style={styles.iconLocalization} source={{uri: 'https://cdn-icons-png.flaticon.com/512/17/17589.png'}} />
+                    <Text style={[styles.text, styles.textLocalization, styles.textBlack]}>Accra, Ghana</Text>
+                </View>
+                <Text style={[styles.textSmall, styles.textBlack]}>20-Sep-22</Text>
             </View>
-            <View style={styles.cabecalho}>
-                <Text>55</Text>
-                <Text>5.00 mph</Text>
-                <Text>30%</Text>
-                <Text>1023.1 pa</Text>
+
+            <View style={styles.card}>
+                <View style={styles.element}>
+                    <Text style={[styles.temperature, styles.textBlack]}>20°</Text>
+                    <Text style={[styles.textSmall, styles.textBlack]}>Real feel: 20</Text>
+                </View>
+
+                <View style={styles.element}>
+                    <Image style={styles.imgWeather} source={{uri: 'https://cdn-icons-png.flaticon.com/512/8841/8841362.png'}} />
+                    <Text style={[styles.textWeather, styles.textBlack]}>Cloudy</Text>
+                </View>
+            </View>
+
+            <View style={styles.content}>
+                <View style={styles.data}>
+                    <Text style={styles.textData}>UV Index</Text>
+                    <Text style={styles.textData}>Humidity</Text>
+                    <Text style={styles.textData}>Wind Speed</Text>
+                    <Text style={styles.textData}>Rain Probability</Text>
+                    <Text style={styles.textData}>Pressure</Text>
+                </View>
+                
+                <View style={styles.data}>
+                    <Text style={styles.textData}></Text>
+                    <Text style={styles.textData}>55</Text>
+                    <Text style={styles.textData}>5.00 mph</Text>
+                    <Text style={styles.textData}>30%</Text>
+                    <Text style={styles.textData}>1023.1 pa</Text>
+                </View>
             </View>
         </View>
-    </View>
-    // <View style={styles.container}>
-    //   <Text>Weather!</Text>
-    // </View>
-  );
+    );
 }
 
 function Metrics() {
-  return (
-    <View style={styles.container}>
-      <Text>Metrics!</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Metrics!</Text>
+        </View>
+    );
 }
 
 function News() {
-  return (
-    <View style={styles.container}>
-      <Text>News!</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>News!</Text>
+        </View>
+    );
 }
 
 function Localization() {
-  return (
-    <View style={styles.container}>
-      <Text>Localization!</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Localization!</Text>
+        </View>
+    );
 }
 
 const Tab = createBottomTabNavigator();
@@ -68,37 +78,37 @@ const Tab = createBottomTabNavigator();
 export default function navigationBar() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
 
-            if (route.name === 'Weather') {
-              iconName = focused ? 'partly-sunny' : 'partly-sunny-outline';
-            }
-            else if (route.name === 'Metrics') {
-              iconName = focused ? 'analytics' : 'analytics-outline';
-            }
-            else if (route.name === 'News') {
-              iconName = focused ? 'ios-newspaper' : 'ios-newspaper-outline';
-            }
-            else if (route.name === 'Localization') {
-              iconName = focused ? 'location-outline' : 'location-outline';
-            }
+                    if (route.name === 'Weather') {
+                        iconName = focused ? 'cloudy-sharp' : 'cloudy-outline';
+                    }
+                    else if (route.name === 'Metrics') {
+                        iconName = focused ? 'analytics' : 'analytics-outline';
+                    }
+                    else if (route.name === 'News') {
+                        iconName = focused ? 'ios-newspaper' : 'ios-newspaper-outline';
+                    }
+                    else if (route.name === 'Localization') {
+                        iconName = focused ? 'location-sharp' : 'location-outline';
+                    }
 
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        {/* <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarBadge: 3 }} /> */}
-        <Tab.Screen name="Weather" component={Weather} />
-        <Tab.Screen name="Metrics" component={Metrics} />
-        <Tab.Screen name="News" component={News} />
-        <Tab.Screen name="Localization" component={Localization} />
-      </Tab.Navigator>
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: '#8DBBFF',
+                tabBarInactiveTintColor: 'gray',
+            })}
+        >
+            {/* <Tab.Screen name="Home" component={HomeScreen} options={{ tabBarBadge: 3 }} /> */}
+            <Tab.Screen name="Weather" component={Weather} />
+            <Tab.Screen name="Metrics" component={Metrics} />
+            <Tab.Screen name="News" component={News} />
+            <Tab.Screen name="Localization" component={Localization} />
+        </Tab.Navigator>
     </NavigationContainer>
   );
 }
--- react/hello_word/app/style.js
+++ react/hello_word/app/style.js
@@ -4,13 +4,99 @@ export const styles = StyleSheet.create ({
     container: {
         flex: 1, 
         alignItems: 'center', 
-        justifyContent: 'center'
+        justifyContent: 'space-around'
     },
-    cabecalho: {
-        alignItems: 'center',
-    },
-    data: {
+
+    header: {
         alignItems: 'center',
         justifyContent: 'center',
-    }
+    },
+    
+    location: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    
+    textBlack: {
+        color: 'black',
+    },
+    
+    iconLocalization: {
+        width: 14,
+        height: 14,
+        marginRight: 5,
+    },
+
+    textLocalization: {
+        fontWeight: "bold",
+    },
+    
+    textSmall: {
+        fontSize: 12,
+    },
+    
+    text: {
+        fontSize: 14,
+    },
+
+    card: {
+        backgroundColor: 'white',
+        padding: 30,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        
+        borderRadius: 20,
+        elevation: 20,
+        shadowColor: 'grey',
+
+        width: 330,
+    },
+
+    element: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+        height: 130,
+    },
+
+    temperature: {
+        fontSize: 55,
+    },
+    
+    textWeather: {
+        fontSize: 20,
+    },
+
+    imgWeather: {
+        width: 65,
+        height: 65,
+    },
+
+    content: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+
+        width: 230,
+    },
+
+    data: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+
+        height: 150,
+    },
+
+    textData: {
+        fontWeight: "bold",
+        fontSize: 14,
+    },
 });
